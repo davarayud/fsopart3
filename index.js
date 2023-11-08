980a9274208a3752ac5b0d194c3d426a2962a5b0
unknown <davarayud@gmail.com>
2023-11-08 10:27:08 -0300

    add exercises 3.8

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-
-app.use(morgan('tiny'))
 app.use(express.json())
+morgan.token('bodypost', (request) => {
+ // console.log(request)
+  if(request.method === "POST") {
+    return JSON.stringify(request.body)
+  }
+  return " "
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :bodypost'))
+
 
 let persons = [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
-    }
-  ]
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
+]
 
 app.get('/info', (request, response) => {
-    const date = Date()
-    console.log(date)
-    response.send(
-      `<p>Phonebook has info for ${persons.length} people</p>
+  const date = Date()
+  console.log(date)
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
        <p>${date}</p>`
-    )
+  )
 })
 
 app.get('/api/persons', (request, response) => {
@@ -47,7 +55,7 @@ app.get('/api/persons/:id', (request, response) => {
 
   if (person) {
     response.json(person)
-  }else{
+  } else {
     response.status(404).end()
   }
 })
@@ -59,17 +67,17 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const generateID = () => Number.parseInt(Math.random()*1000000000)
+const generateID = () => Number.parseInt(Math.random() * 1000000000)
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if(!body.name || !body.number){
+  if (!body.name || !body.number) {
     return response.status(400).json({
       error: 'name or number is missing'
     })
-  } 
-  if(persons.filter(person => person.name === body.name).length) {
+  }
+  if (persons.filter(person => person.name === body.name).length) {
     return response.status(400).json({
       error: 'name must be unique'
     })
@@ -87,5 +95,5 @@ app.post('/api/persons', (request, response) => {
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
